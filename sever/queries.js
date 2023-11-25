af0d24405fe70fc0abfc0f02656444575b783e9f
HiHocCode <125520970+Hi-hoc-code@users.noreply.github.com>
2023-11-25 23:45:51 +0700

    connectionString

--- sever/queries.js
+++ sever/queries.js
@@ -1,10 +1,19 @@
+// const Pool = require('pg').Pool
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'staff',
+//   password: 'mysecretpassword',
+//   port: 5432,
+// })
+
+// module.exports = {pool}
+
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'staff',
-  password: 'mysecretpassword',
-  port: 5432,
+    connectionString:'postgresql://Hi-hoc-code:IeAo42GHciEx@ep-lucky-voice-99485450-pooler.ap-southeast-1.aws.neon.tech/staff?sslmode=require',
+      ssl: {
+        require: true,
+      },
 })
-
 module.exports = {pool}
