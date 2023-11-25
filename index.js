c8294f45e1878ef4dbc2ebd881a1a79092d43d32
HiHocCode <125520970+Hi-hoc-code@users.noreply.github.com>
2023-11-25 23:57:25 +0700

    createUser

--- index.js
+++ index.js
@@ -28,3 +28,13 @@ const getUsers = (request, response) => {
 }
 app.get('/staff', getUsers);
 
+const createUser = (request, response) => {
+    const { name, email } = request.body
+    pool.query('INSERT INTO staff(name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`User added with ID: ${results.insertId}`)
+    })
+  }
+app.post('/staff', createUser);
